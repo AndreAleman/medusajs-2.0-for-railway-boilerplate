5707b281ae85341243c5dc2f69e4a0e253b03913
AndreAleman <davidandrealeman@gmail.com>
2025-09-14 22:50:44 +0100

    Add favicon and og image

--- storefront/src/app/[countryCode]/(main)/layout.tsx
+++ storefront/src/app/[countryCode]/(main)/layout.tsx
@@ -6,6 +6,29 @@ import { getBaseURL } from "@lib/util/env"
 
 export const metadata: Metadata = {
   metadataBase: new URL(getBaseURL()),
+  title: "Stainless Steel Tubing, Fittings, and Valves | Cowbird Depot",
+  description: "Your premium source for stainless steel tubing, fittings, and valves",
+  openGraph: {
+    title: "Stainless Steel Tubing, Fittings, and Valves | Cowbird Depot",
+    description: "Your premium source for stainless steel tubing, fittings, and valves",
+    url: getBaseURL(),
+    siteName: "Cowbird Depot",
+    images: [
+      {
+        url: "images/og-image.jpeg", // Place your image in the public folder
+        width: 1200,
+        height: 630,
+        alt: "Cowbird Depot - Stainless Steel Products",
+      },
+    ],
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Stainless Steel Tubing, Fittings, and Valves | Cowbird Depot",
+    description: "Your premium source for stainless steel tubing, fittings, and valves",
+    images: ["/og-image.jpg"],
+  },
 }
 
 export default async function PageLayout(props: { children: React.ReactNode }) {
